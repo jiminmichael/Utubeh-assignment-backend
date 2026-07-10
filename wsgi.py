867dd77476359e1eecd6379ce9a474a17a5ef698
jiminmichael <114304392+jiminmichael@users.noreply.github.com>
2026-07-11 00:58:59 +0100

    Create asset_management

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-﻿"""WSGI config for asset_management project."""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asset_management.settings")
-
-application = get_wsgi_application()
